f005a9000e9006bf0f64c53d6c45b907d5f0bb30
ayl4408 <ayl4408@gmail.com>
2016-02-15 23:57:30 -0500

    repositioned the print statement and used imrazz if statement for the key error

--- front/home.py
+++ front/home.py
@@ -4,13 +4,13 @@
 import Cookie, os, LINK_HEADERS
 from templite import Templite
 
-print "Content-Type: text/html\r\n\r\n"
-
 def m(username):
     f = open("home.tpl","r")
     html = str(f.read())
     f.close()
-    
+
+    print "Content-Type: text/html\r\n\r\n"
+
     t = Templite(html)
     print t.render(username=username, login_link=LINK_HEADERS.LOGIN_LINK, home_link=LINK_HEADERS.HOME_LINK, transaction_link=LINK_HEADERS.TRANSACTION_LINK, deposit_link=LINK_HEADERS.DEPOSIT_LINK, upload_link=LINK_HEADERS.UPLOAD_LINK, dropdown_link=LINK_HEADERS.DROPDOWN_LINK, portfolio_link=LINK_HEADERS.PORTFOLIO_LINK)
 
@@ -18,7 +18,7 @@ def check_cookie():
     cookie = Cookie.SimpleCookie()
     cookie_string = Cookie.SimpleCookie(os.environ.get('HTTP_COOKIE'))
 
-    if cookie_string != None:
+    if (cookie_string != None) and ("login" in cookie_string):
         cookie.load(cookie_string)
         login_result = str(cookie['login'].value)
     
