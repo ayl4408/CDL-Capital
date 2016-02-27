e3ede97646ace4414c2d693a9e1b3eb7ce50df4d
ayl4408 <ayl4408@gmail.com>
2016-02-27 16:26:32 -0500

    history model

---
+++ models/history_model.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+class History:
+    def __init__(self, user, trans_date, trans_type, stock, price, total_price, volume):
+        self.user = user
+        self.trans_date = trans_date
+        self.trans_type = trans_type
+        self.stock = stock
+        self.price = price
+        self.total_price = total_price
+        self.volume = volume
+
+    def get_user(self):
+        return self.user
+
+    def set_user(self,x):
+        self.user = x
+
+    def get_trans_date(self):
+        return self.trans_date
+
+    def set_trans_date(self,x):
+        self.trans_date = x
+
+    def get_trans_type(self):
+        return self.trans_type
+
+    def set_trans_type(self,x):
+        self.trans_type = x
+
+    def get_stock(self):
+        return self.stock
+
+    def set_stock(self,x):
+        self.stock = x
+
+    def get_price(self):
+        return self.price
+
+    def set_price(self,x):
+        self.price = x
+
+    def get_total_price(self):
+        return self.total_price
+
+    def set_total_price(self,x):
+        self.total_price = x
+
+    def get_volume(self):
+        return self.volume
+
+    def set_volume(self,x):
+        self.volume = x
+
+    
+        
