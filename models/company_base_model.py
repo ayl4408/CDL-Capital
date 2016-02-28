f054d2ecb7e27ab468dabe0899c23ec386d39425
ayl4408 <ayl4408@gmail.com>
2016-02-27 22:59:17 -0500

    company_base_model

---
+++ models/company_base_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+class Company_base:
+
+    def __init__(self):
+        name = None
+        ask = None
+        symbol = None
+
+    def get_name():
+        return self.name
+
+    def set_name(self, x):
+        self.name = x
+
+    def get_ask():
+        return self.ask
+
+    def set_ask(self, x):
+        self.ask = x
+
+    def get_symbol():
+        return self.symbol
+
+    def set_symbol(self, x):
+        self.symbol = x
+
+    
