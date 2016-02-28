202988f2ab1338e5683096763e7929533d3d90f2
ayl4408 <ayl4408@gmail.com>
2016-02-27 23:38:12 -0500

    select all stock/ company base model

--- dao/transaction_dao.py
+++ dao/transaction_dao.py
@@ -16,6 +16,15 @@ class Transaction_dao:
 
      def __init__(self):
           self.db =  PDO().get_connection(LINK_HEADERS.DB_HOST, LINK_HEADERS.DB_USER, LINK_HEADERS.DB_PASSWORD, LINK_HEADERS.DB_NAME)
+
+     def select_all_stock(self):
+          result = self.db.query("select Distinct stock from transactions;")
+          if result:
+               l = []
+               for i in range(len(result)):
+                    t = Transaction(None, None, result[i]['stock'], None, None, None, None)
+                    l.append(t)
+               return l
           
      def select_all(self, user):
           result = self.db.query("select * from transactions where user=('%s')"%(user)+";")
--- models/company_base_model.py
+++ models/company_base_model.py
@@ -7,19 +7,19 @@ class Company_base:
         ask = None
         symbol = None
 
-    def get_name():
+    def get_name(self):
         return self.name
 
     def set_name(self, x):
         self.name = x
 
-    def get_ask():
+    def get_ask(self):
         return self.ask
 
     def set_ask(self, x):
         self.ask = x
 
-    def get_symbol():
+    def get_symbol(self):
         return self.symbol
 
     def set_symbol(self, x):
