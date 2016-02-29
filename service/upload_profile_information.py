f83b64c77d27bdd55d87b4da429d9ffb8f734c27
ayl4408 <ayl4408@gmail.com>
2016-02-29 14:37:40 -0500

    fix portfolio information

--- service/upload_profile_information.py
+++ service/upload_profile_information.py
@@ -4,6 +4,7 @@ import cgi, sys, LINK_HEADERS
 import simplejson as json
 from datetime import datetime
 from dateutil import tz
+from decimal import *
 sys.path.insert(0, str(LINK_HEADERS.DAO_LINK))
 from transaction_dao import Transaction_dao
 from user_portfolio_dao import User_portfolio_dao
@@ -18,7 +19,7 @@ form = cgi.FieldStorage()
 if form.getvalue("username") != None:
     username = form.getvalue("username")
 
-#username='al356'
+username='al356'
     
 tdao = Transaction_dao()
 u2 = User_stock_value_dao()
@@ -26,44 +27,28 @@ u1 = User_portfolio_dao()
 cdao = Company_dao()
 hdao = History_dao()
 
-usv = tdao.get_user_stock_value_model(username)
-up = u1.get_user_portfolio_model(username)
-
 data={}
-
-data['users']={}
-data['users']['profit'] = usv.get_profit()
-data['users']['total_portfolio'] = up.get_total_portfolio()
-data['users']['available_funds'] = up.get_available_funds()
-data['users']['total_stock_values'] = usv.get_total_stock_values()
-data['users']['total_deposited'] = up.get_total_deposited()
 
 t = hdao.select_all(username)
 
 if t:
     data['transactions']={}
+
     for i in range(len(t)):
         data['transactions'][i]={}
 	
 	#start date formatting
 	from_zone = tz.tzutc()
 	to_zone = tz.tzlocal()
-
         date_time = t[i].get_trans_date()
-
 	date_time = date_time.strftime('%Y-%m-%d %H:%M:%S')
 	date_time = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')	
-
 	date_time = date_time.replace(tzinfo=from_zone)
 	updated_date_time = date_time.astimezone(to_zone)
-
-	
 	updated_date_time = updated_date_time.strftime('%Y-%m-%d %H:%M:%S')
 	#end date formatting	
 
         data['transactions'][i]['trans_date'] = updated_date_time
-
-
         data['transactions'][i]['trans_type'] = t[i].get_trans_type()
         data['transactions'][i]['stock'] = t[i].get_stock()
         data['transactions'][i]['price'] = t[i].get_price()
@@ -80,9 +65,11 @@ else:
     data['transactions'][0]['volume'] = ""
     
 l = tdao.get_user_stock_list(username)
-        
+
 if l:
     data['owned_stocks']={}
+    total_stock_value = 0
+    
     for i in range(len(l)):
         c = cdao.get_company_model(l[i])
         o = tdao.get_owned_stock_model(username, l[i], c.get_ask()) 
@@ -92,6 +79,11 @@ if l:
         data['owned_stocks'][i]['current_price'] = c.get_ask()
         data['owned_stocks'][i]['total_worth'] = o.get_total_worth()
         data['owned_stocks'][i]['profit'] = o.get_profit()
+        total_stock_value = Decimal(total_stock_value) + Decimal(o.get_total_worth())
+
+    u2.update_total_stock_values(username, total_stock_value)
+    u = u1.get_user_portfolio_model(username)
+    u1.update_total_portfolio(username, Decimal(u.get_available_funds()) + total_stock_value)
 else:
     data['owned_stocks']={}
     data['owned_stocks'][0]={}
@@ -100,7 +92,16 @@ else:
     data['owned_stocks'][0]['current_price'] = ""
     data['owned_stocks'][0]['total_worth'] = ""
     data['owned_stocks'][0]['profit'] = ""
+
+usv = u2.get_user_stock_value_model(username)
+up = u1.get_user_portfolio_model(username)
+
+data['users']={}
+data['users']['profit'] = usv.get_profit()
+data['users']['total_portfolio'] = up.get_total_portfolio()
+data['users']['available_funds'] = up.get_available_funds()
+data['users']['total_stock_values'] = usv.get_total_stock_values()
+data['users']['total_deposited'] = up.get_total_deposited()
     
 json_result = json.dumps(data)
 print json_result
-
