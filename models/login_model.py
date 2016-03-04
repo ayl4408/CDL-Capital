cd2234ad414ffe2347994259d2eb17183f865459
ayl4408 <ayl4408@gmail.com>
2016-03-04 18:22:21 -0500

    login model

---
+++ models/login_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+class Login:
+
+    def __init__(self, user):
+        self.user = user
+
+    def set_user(self, x):
+        self.user = x
+
+    def get_user(self):
+        return self.user
